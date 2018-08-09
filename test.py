d9ce78f4c8da444e5f75653c9cdf90a7f4bd1074
hvanzyl <hvanzyl@motiondesign.co.nz>
2018-08-09 14:35:27 +1200

    refactored code to make it readable

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
-import sys; x = 'Hello, World!'; sys.stdout.write(x + '\n')
+import sys; 
+
+x = 'Hello, World!'; 
+sys.stdout.write(x + '\n')
 
 raw_input("\n\nPress the enter key to exit")
